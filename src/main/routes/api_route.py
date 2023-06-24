fa77ccdd64065e993e40c193d24cd77dcd516373
Carlos Eduardo - Kadu <carlos.spadilha@yahoo.com.br>
2023-06-24 02:33:46 -0300

    feat: inserindo templates e static

--- src/main/routes/api_route.py
+++ src/main/routes/api_route.py
@@ -4,7 +4,25 @@ api_routes_bp = Blueprint("api_routes", __name__)
 
 
 @api_routes_bp.route("/", methods=["GET"])
-def teste():
+def home():
     """rota teste"""
 
-    return render_template("teste.html")
+    apelido = "Carol"
+    quantidade_carrinho = 3
+    return render_template(
+        "base.html", apelido=apelido, quantidade_carrinho=quantidade_carrinho
+    )
+
+
+@api_routes_bp.route("/login", methods=["GET"])
+def login():
+    """tela login"""
+
+    return render_template("login.html", quantidade_carrinho=3)
+
+
+@api_routes_bp.route("/teste", methods=["GET"])
+def teste():
+    """tela teste"""
+
+    return render_template("cadastrar_cliente.html")
