541ab4e8eb798249078e02713aed342acc808c02
Carlos Eduardo - Kadu <carlos.spadilha@yahoo.com.br>
2023-07-06 23:14:49 -0300

    feat: iniciando a pagina carrinho

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from src.main.composer.buscar_cep_composer import buscar_cep_composer
 from src.presenters.helpers.http_models import HttpRequest
 
 
-http_request = HttpRequest(query={"cep_cliente": "26515570"})
+http_request = HttpRequest(query={"cep_cliente": "28984350"})
 response = buscar_cep_composer()
 response = response.route_buscar_cep(http_request)
 
