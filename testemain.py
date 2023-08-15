9b52b8467f55015da95aa795ca2f2dc22cfe5c86
Carlos Eduardo - Kadu <carlos.spadilha@yahoo.com.br>
2023-08-14 21:42:48 -0300

    feat: acertando telas de front end

--- testemain.py
+++ testemain.py
@@ -1,6 +1,6 @@
 from src.main.configs.app import app
 from src.infra.config import DBConnectionHandler, Base
-import os
+
 
 # criando o banco de dados.
 db_conn = DBConnectionHandler()
@@ -8,4 +8,4 @@ engine = db_conn.get_engine()
 Base.metadata.create_all(engine)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)), debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
